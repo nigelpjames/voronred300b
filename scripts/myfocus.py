b017e8ee8459578afe521f133513dc386d5ab34b
Nigel James <nigel@james.gen.nz>
2022-05-28 13:56:26 +1200

    Backup triggered on 2022-05-28 13:56:26
    Klipper on commit: af38d70
    
    Moonraker on commit: d53cf27
    
    Fluidd version: v1.18.1

--- scripts/myfocus.py
+++ scripts/myfocus.py
@@ -5,6 +5,6 @@ from ctypes import *
 arducam_vcm = CDLL('/home/pi/arducam/Motorized_Focus_Camera/python/lib/libarducam_vcm.so')
 
 arducam_vcm.vcm_init()
-focus=320
+focus=310
 print("Setting arducam focus to ",focus)
 arducam_vcm.vcm_write(focus)
