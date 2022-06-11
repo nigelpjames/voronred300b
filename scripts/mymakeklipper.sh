e7a6e3b7a5465ac660a4e76817ec7b0483fad938
nigelpjames <36124687+nigelpjames@users.noreply.github.com>
2022-06-12 10:50:07 +1200

    Add files via upload

--- scripts/mymakeklipper.sh
+++ scripts/mymakeklipper.sh
@@ -17,22 +17,22 @@ cd ~/klipper
 echo "Updating Klipper on RP MCU [Input Shaper]"
 
 echo Using make override [${rp_mcu}]
-make clean KCONFIG_NAME=${rp_mcu}
-make menuconfig KCONFIG_NAME=${rp_mcu}
-make KCONFIG_NAME=${rp_mcu}
+make clean KCONFIG_CONFIG=${rp_mcu}
+#make menuconfig KCONFIG_CONFIG=${rp_mcu}
+make KCONFIG_CONFIG=${rp_mcu}
 read -p "Review errors and press [Enter] to continue flashing or Ctrl-C to abort"
-make flash KCONFIG_NAME=${rp_mcu}
+make flash KCONFIG_CONFIG=${rp_mcu}
 echo "RP MCU [Input Shaper] updated"
 echo 
 
 echo "Updating Klipper on Spider MCU"
 
 echo Using make override [${spider_mcu}] to flash ${usb_mcu}
-make clean KCONFIG_NAME=${spider_mcu}
-make menuconfig KCONFIG_NAME=${spider_mcu}
-make KCONFIG_NAME=${spider_mcu}
+make clean KCONFIG_CONFIG=${spider_mcu}
+#make menuconfig KCONFIG_CONFIG=${spider_mcu}
+make KCONFIG_CONFIG=${spider_mcu}
 read -p "Review errors and press [Enter] to continue flashing or Ctrl-C to abort"
-make flash KCONFIG_NAME=${spider_mcu} FLASH_DEVICE=${usb_mcu} 
+make flash KCONFIG_CONFIG=${spider_mcu} FLASH_DEVICE=${usb_mcu} 
 echo "Spider MCU updated"
 echo 
 
