8c4c7a903061365664e41f98ac1bbdf8a8a7f25c
Nigel James <nigel@james.gen.nz>
2022-06-21 18:29:23 +1200

    Backup triggered on 2022-06-21 18:29:23
    Klipper on commit: 7f9ea23
    
    Moonraker on commit: a8b9fc0
    
    Fluidd version: v1.18.2

--- scripts/myinputshaper.sh
+++ scripts/myinputshaper.sh
@@ -4,14 +4,16 @@
 
 if ls /tmp/resonances_x_*.csv >/dev/null 2>&1; then
    python3 ~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o ~/klipper_config/shaper_calibrate_x.png
+   rm -f /tmp/resonances_x_*.csv
 else
    echo No ADXL input shaper data file for X axis
-   echo Run TEST_RESONANCES AXIS=X first
+   echo Run TEST_RESONANCES AXIS=X to generate
 fi
 
 if ls /tmp/resonances_y_*.csv >/dev/null 2>&1; then
    python3 ~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o ~/klipper_config/shaper_calibrate_y.png
+   rm -f /tmp/resonances_y_*.csv
 else
    echo No ADXL input shaper data file for Y axis
-   echo Run TEST_RESONANCES AXIS=Y first
+   echo Run TEST_RESONANCES AXIS=Y to generate
 fi
